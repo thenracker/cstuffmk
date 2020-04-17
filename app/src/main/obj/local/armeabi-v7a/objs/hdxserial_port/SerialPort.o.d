b786e7d0ba8d28e06861c87b940e9ac200436a10
Petr Weissar <weissar.petr@gmail.com>
2020-04-17 12:14:01 +0200

    Edit for rockchip purposes only

--- app/src/main/obj/local/armeabi-v7a/objs/hdxserial_port/SerialPort.o.d
+++ app/src/main/obj/local/armeabi-v7a/objs/hdxserial_port/SerialPort.o.d
@@ -1,5 +1,5 @@
-C:/Users/petrw/AndroidStudioProjects/cstuffmk/app/src/main/obj/local/armeabi-v7a/objs/hdxserial_port/SerialPort.o: \
-  C:\Users\petrw\AndroidStudioProjects\cstuffmk\app\src\main\jni\SerialPort.c \
-  C:\Users\petrw\AndroidStudioProjects\cstuffmk\app\src\main\jni\SerialPort.h
+C:/Users/petrw/AndroidStudioProjects/cstuff-git/app/src/main/obj/local/armeabi-v7a/objs/hdxserial_port/SerialPort.o: \
+  C:\Users\petrw\AndroidStudioProjects\cstuff-git\app\src\main\jni\SerialPort.c \
+  C:\Users\petrw\AndroidStudioProjects\cstuff-git\app\src\main\jni\SerialPort.h
 
-C:\Users\petrw\AndroidStudioProjects\cstuffmk\app\src\main\jni\SerialPort.h:
+C:\Users\petrw\AndroidStudioProjects\cstuff-git\app\src\main\jni\SerialPort.h:
